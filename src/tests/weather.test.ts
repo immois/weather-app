a183d4cc7a1498f9b8315fdb6eccd97eac3d7cf4
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-03-08 07:39:35 -0500

    test: edit test description getWeatherIcon

--- src/tests/weather.test.ts
+++ src/tests/weather.test.ts
@@ -1,17 +1,17 @@
 import { ICON_DEFAULT, ICON_LIST, getWeatherIcons } from "@/lib/weather";
 
-describe("funcion que retornar la ruta de un icono svg segun el icono de weather", () => {
-  test("debe retornar un string", () => {
+describe("getWeatherIcons test", () => {
+  test("return a string", () => {
     const result = getWeatherIcons("01d");
     expect(typeof result).toBe("string");
   });
 
-  test("debe retornar la ruta del icono svg", () => {
+  test("return the icon path svg", () => {
     const result = getWeatherIcons("10n");
     expect(result).toBe(ICON_LIST["10n"]);
   });
 
-  test("debe retornar la ruta por default", () => {
+  test("return default path", () => {
     const result = getWeatherIcons("12d");
     expect(result).toBe(ICON_DEFAULT);
   });
