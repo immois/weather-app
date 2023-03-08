203634b133a3958bdfc5fef684d06128e632347e
Rolando Machuca Valverde <rmoimv@gmail.com>
2023-03-08 17:48:10 -0500

    feat: update place component

--- src/components/place/place.tsx
+++ src/components/place/place.tsx
@@ -1,11 +1,9 @@
-import { PlaceProps } from "@/types/place";
 import styles from "./place.module.css";
 
-export const Place = ({ city, country }: PlaceProps) => {
+export const Place = ({ city_name }: { city_name: string }) => {
   return (
     <div className={styles.place}>
-      <h2 className={styles.city}>{city}</h2>
-      <p className={styles.country}>{country}</p>
+      <h2 className={styles.city}>{city_name}</h2>
     </div>
   );
 };
